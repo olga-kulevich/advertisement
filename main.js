11c4cb75e3ca46b680a3d69a9afe3b029dea2681
olga <ol4ik_bel89@mail.ru>
2018-08-29 20:39:04 +0300

    working navigation

--- main.js
+++ main.js
@@ -96,4 +96,22 @@ function getPdf() {
         });
 };
 
+const listItems = document.querySelectorAll('#list-menu li a');
+const listContainers = document.querySelectorAll('.content');
+
+for (var i = 0; i < listItems.length; i++) {
+    var listItem = listItems[i];
+    listItem.addEventListener('click', function () {
+        for (var j = 0; j < listItems.length; j++) {
+            var item = listItems[j];
+            var container = listContainers[j];
+            item.classList.remove('active');
+            container.style.display = 'none';
+        }
+        event.currentTarget.classList.add('active');
+        var containerId = event.currentTarget.id;
+        document.getElementById('container-' + containerId).style.display = "block";
+    });
+}
+
 document.addEventListener('DOMContentLoaded', createPreview);
