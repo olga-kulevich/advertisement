261ff7617f2d0bc886eea4b767fe9e2b9b3cebc8
olga <ol4ik_bel89@mail.ru>
2018-09-01 13:47:48 +0300

    exported preview loading scripts to preview.js

--- main.js
+++ main.js
@@ -1,26 +1,20 @@
 import States from './states.js';
 import initMenu from './menu.js';
-import putLoadedImageOnPreview from './preview.js';
+import {putLoadedImageOnPreview, createPreview} from './preview.js';
 
 initMenu();
 
 const preview = document.getElementById('preview'),
     previewBig = document.getElementById('previewBig'),
     photo = document.getElementById('photo'),
-    title = document.getElementById('title'),
     selectTitle = document.getElementById('select-title'),
     selectAnimal = document.getElementById('animal'),
-    description = document.getElementById('description'),
     areaMainChar = document.getElementById('area-main-characteristics'),
     conditions = document.getElementById('conditions'),
     areaConditions = document.getElementById('area-conditions'),
-    description2 = document.getElementById('description2'),
     areaDescriptAnimal = document.getElementById('area-description-animal'),
-    behavioralFeatures = document.getElementById('behavioral-features'),
     areaBehavioralFeatures = document.getElementById('area-behavioral-features'),
-    firstPhonenumber = document.getElementById('first-phonenumber'),
     firstInputPhonenumber = document.getElementById('first-input-phonenumber'),
-    secondPhonenumber = document.getElementById('second-phonenumber'),
     secondInputPhonenumber = document.getElementById('second-input-phonenumber'),
     checkbox = document.getElementById('inlineCheckbox1'),
     fee = document.getElementById('fee'),
@@ -120,41 +114,6 @@ areaBehavioralFeatures.addEventListener('input', debounce(pushBehavFeatures, 300
 firstInputPhonenumber.addEventListener('input', debounce(pushFirstPhone, 300));
 secondInputPhonenumber.addEventListener('input', debounce(pushSecondPhone, 300));
 sumFee.addEventListener('input', debounce(pushFee, 300));
-
-function createPreview() {
-    putLoadedImageOnPreview();
-
-    title.innerHTML = selectTitle.options[selectTitle.selectedIndex].value.toUpperCase() + ' ' + selectAnimal.value.toUpperCase();
-    description.innerHTML = areaMainChar.value;
-    conditions.innerHTML = areaConditions.value;
-    description2.innerHTML = areaDescriptAnimal.value;
-    behavioralFeatures.innerHTML = areaBehavioralFeatures.value;
-    firstPhonenumber.innerHTML = firstInputPhonenumber.value;
-    secondPhonenumber.innerHTML = secondInputPhonenumber.value;
-
-    if (checkbox.checked) {
-        fee.innerHTML = checkbox.value + ' ' + sumFee.value + '!';
-    } else {
-        fee.innerHTML = "";
-    }
-
-    $('#previewBig').show(0);
-
-    domtoimage.toSvg(previewBig)
-        .then(function (dataUrl) {
-            while (preview.firstChild) {
-                preview.removeChild(preview.firstChild);
-            }
-
-            $('#previewBig').hide();
-            var img = new Image();
-            img.src = dataUrl;
-            img.classList.add("img-fluid", "img-responsive");
-
-            preview.appendChild(img);
-            preview.style.opacity = 1;
-        });
-}
 
 export function getScreenshot() {
     $('#previewBig').show(0);
--- preview.js
+++ preview.js
@@ -1,12 +1,54 @@
-export default function putLoadedImageOnPreview() {
+export function putLoadedImageOnPreview() {
     var petImage = document.getElementById('pet-image');
     var file = document.getElementById('photo').files[0];
     var reader = new FileReader();
 
     if (file && document.getElementById('photo').files.length !== 0) {
-        reader.onloadend = function() {
+        reader.onloadend = function () {
             petImage.src = reader.result;
         };
         reader.readAsDataURL(file);
     }
+}
+
+const title = document.getElementById('title'),
+    description = document.getElementById('description'),
+    description2 = document.getElementById('description2'),
+    behavioralFeatures = document.getElementById('behavioral-features'),
+    firstPhonenumber = document.getElementById('first-phonenumber'),
+    secondPhonenumber = document.getElementById('second-phonenumber');
+
+export function createPreview() {
+    putLoadedImageOnPreview();
+
+    title.innerHTML = selectTitle.options[selectTitle.selectedIndex].value.toUpperCase() + ' ' + selectAnimal.value.toUpperCase();
+    description.innerHTML = areaMainChar.value;
+    conditions.innerHTML = areaConditions.value;
+    description2.innerHTML = areaDescriptAnimal.value;
+    behavioralFeatures.innerHTML = areaBehavioralFeatures.value;
+    firstPhonenumber.innerHTML = firstInputPhonenumber.value;
+    secondPhonenumber.innerHTML = secondInputPhonenumber.value;
+
+    if (checkbox.checked) {
+        fee.innerHTML = checkbox.value + ' ' + sumFee.value + '!';
+    } else {
+        fee.innerHTML = "";
+    }
+
+    $('#previewBig').show(0);
+
+    domtoimage.toSvg(previewBig)
+        .then(function (dataUrl) {
+            while (preview.firstChild) {
+                preview.removeChild(preview.firstChild);
+            }
+
+            $('#previewBig').hide();
+            var img = new Image();
+            img.src = dataUrl;
+            img.classList.add("img-fluid", "img-responsive");
+
+            preview.appendChild(img);
+            preview.style.opacity = 1;
+        });
 }
