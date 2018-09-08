e9e689550f88995fa82f11eda6c196f6b4ad36a8
olga <ol4ik_bel89@mail.ru>
2018-09-08 22:39:57 +0300

    added text block to header

--- menu.js
+++ menu.js
@@ -2,6 +2,7 @@ export default function initMenu() {
 
     const listItems = document.querySelectorAll('#list-menu li a');
     const listContainers = document.querySelectorAll('.content');
+    const listHeaders = document.querySelectorAll('.text-in-head');
 
     for (var i = 0; i < listItems.length; i++) {
         var listItem = listItems[i];
@@ -9,12 +10,16 @@ export default function initMenu() {
             for (var j = 0; j < listItems.length; j++) {
                 var item = listItems[j];
                 var container = listContainers[j];
+                var head = listHeaders[j];
                 item.classList.remove('active');
                 container.style.display = 'none';
+                head.style.display = 'none';
             }
             event.currentTarget.classList.add('active');
             var containerId = event.currentTarget.id;
+            var headId = event.currentTarget.id;
             document.getElementById('container-' + containerId).style.display = "block";
+            document.getElementById('header-' + headId).style.display = "block";
         });
     }
 }
