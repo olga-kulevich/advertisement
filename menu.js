831a20b10ee8b0dacf76ce5630e836b23cafbcc3
Alexby <alexby@t-rex.by>
2018-09-30 21:36:35 +0300

    Working with url-hash

--- menu.js
+++ menu.js
@@ -4,64 +4,27 @@ export default function initMenu() {
     const listContainers = document.querySelectorAll('.content');
     const listHeaders = document.querySelectorAll('.text-in-head');
 
-    for (var i = 0; i < listItems.length; i++) {
-        var listItem = listItems[i];
-        listItem.addEventListener('click', function (event) {
-            for (var j = 0; j < listItems.length; j++) {
-                var item = listItems[j];
-                var container = listContainers[j];
-                var head = listHeaders[j];
-                item.classList.remove('active');
-                container.style.display = 'none';
-                head.style.display = 'none';
-            }
-            event.currentTarget.classList.add('active');
-            var containerId = event.currentTarget.id;
-            var headId = event.currentTarget.id;
-            document.getElementById('container-' + containerId).style.display = "block";
-            document.getElementById('header-' + headId).style.display = "block";
-        });
-    }
-    
-    function eventFire(el, etype) {
-        if (el.fireEvent) {
-            el.fireEvent('on' + etype);
-        } else {
-        var evObj = document.createEvent('Events');
-        evObj.initEvent(etype, true, false);
-        el.dispatchEvent(evObj);
+    window.onhashchange = function() {
+        window.scrollTo({top: 0});
+        changePage();
+    };
+
+    document.addEventListener('DOMContentLoaded', function() {
+        changePage();
+    });
+
+    const changePage = function() {
+        for (var j = 0; j < listItems.length; j++) {
+            var item = listItems[j];
+            var container = listContainers[j];
+            var head = listHeaders[j];
+            item.classList.remove('active');
+            container.style.display = 'none';
+            head.style.display = 'none';
         }
-}
-    
-    const listLinksMain = document.querySelectorAll('.link-main');
-    for (var i = 0; i < listLinksMain.length; i++) {
-        var listLinkMain = listLinksMain[i];
-        listLinkMain.addEventListener('click', function () {
-           eventFire(document.getElementById('main'), 'click');
-        });
-    }
-    
-    const listLinksConstructor = document.querySelectorAll('.link-constructor');
-     for (var i = 0; i < listLinksConstructor.length; i++) {
-        var listLinkConstructor = listLinksConstructor[i];
-        listLinkConstructor.addEventListener('click', function () {
-           eventFire(document.getElementById('adv'), 'click');
-        });
-    }
-    
-    const listLinksFeedback = document.querySelectorAll('.link-contact-us');
-     for (var i = 0; i < listLinksFeedback.length; i++) {
-        var listLinkFeedback = listLinksFeedback[i];
-        listLinkFeedback.addEventListener('click', function () {
-           eventFire(document.getElementById('feedback'), 'click');
-        });
-    }
-    
-    const listLinksTeam = document.querySelectorAll('.link-team');
-     for (var i = 0; i < listLinksTeam.length; i++) {
-        var listLinkTeam = listLinksTeam[i];
-        listLinkTeam.addEventListener('click', function () {
-           eventFire(document.getElementById('team'), 'click');
-        });
-    }
+        const id = window.location.hash.substr(1);
+        document.getElementById("link-" + id).classList.add('active');
+        document.getElementById('container-' + id).style.display = "block";
+        document.getElementById('header-' + id).style.display = "block";
+    };
 }
